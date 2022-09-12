2c3129779484f017bd760b2ab1f48f06c945d742
Churchill perry <churchill.perry@bryanuniversity.edu>
2022-09-12 00:49:15 -0500

    Functions in Python

---
+++ week1/Functions.py
@@ -0,0 +1,24 @@
+class Functions:
+    def __init__(self):
+        self.list = []
+    def addNum(self, num) -> None:
+        self.list.append(num)
+    def addString(self, str) -> None:
+        self.list.append(str)
+    def addDict(self, dict) -> None:
+        self.list.append(dict)
+    def addTuple(self, tuple) -> None:
+        self.list.extend(tuple)
+    def __str__(self):
+        return str(print(self.list))
+
+myFunc = Functions()
+num = 1
+string = 'the'
+dict = {'a': 'b', 'b': 'c'}
+tuple = (1, 2, 3, 4)
+myFunc.addNum(1)
+myFunc.addString(string)
+myFunc.addDict(dict)
+myFunc.addTuple(tuple)
+print(myFunc)
